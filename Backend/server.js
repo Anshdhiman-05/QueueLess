11aa3519d291343d19905740d975f777685e0683
Anshdhiman-05 <dhimanansh.2004@gmail.com>
2026-07-10 12:19:28 +0530

    Fix model import filename casing

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const User = require("./models/User");
+const User = require("./models/user");
 const Token = require("./models/Token")
 require("dotenv").config();
 
